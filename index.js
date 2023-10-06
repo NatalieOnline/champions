96b7b03f6c09b3e79461a0d3b6cb19c16d25073b
NatalieOnline <137617475+NatalieOnline@users.noreply.github.com>
2023-10-06 14:04:39 +0100

    Updated

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// javascript
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://champs-cf0a1-default-rtdb.europe-west1.firebasedatabase.app"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const enindb = ref(database,"endo")
+
+// let dataToPush = {
+ //   name: "TROLL    ",
+ //   age: "69",
+// }
+// push(enindb,dataToPush)
